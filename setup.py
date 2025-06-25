6c98da2af44a77decf79ecc11e49af627ebccace
C5m7b4 <c5m7b4@gmail.com>
2025-06-24 20:12:29 -0500

    adds version update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='araxia',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     install_requires=[
         'numpy',
