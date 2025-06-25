47883ec6b75da5f438daca742b8632835966156f
C5m7b4 <c5m7b4@gmail.com>
2025-06-24 20:11:17 -0500

    adds train_test_split

--- araxia/utils.py
+++ araxia/utils.py
@@ -19,3 +19,16 @@ def sigmoid(z):
 def sigmoid_derivative(x):
     s = sigmoid(x)
     return s * (1 - s)
+
+def train_test_split(X, y, test_size=0.2):
+    if len(X) != len(y):
+        raise ValueError(f"X and y must have the same length: X: {len(X)}, y: {len(y)}")
+    
+    n = X.shape[0]
+    stop = int(n - (n * test_size))
+    X_train = X.iloc[0:stop, :-1]
+    y_train = y.iloc[0:stop, -1:]
+
+    X_test = X.iloc[stop:, :-1]
+    y_test = y.iloc[stop:, -1:]
+    return X_train, y_train, X_test, y_test
